377a30b92c056ab8aacf7a3c558138b1af2f68c3
Mang Galang <galangpastibisa@gmail.com>
2019-08-01 23:20:52 +0800

    try try try

--- js/main.js
+++ js/main.js
@@ -1,14 +1,13 @@
-var swiper = new Swiper('.upcoming-program .swiper-container', {
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-    });
+var upcomingProgram = new Swiper('.upcoming-program .swiper-container', {
+  navigation: {
+    nextEl: '.upcoming-program .swiper-button-next',
+    prevEl: '.upcoming-program .swiper-button-prev',
+  },
+});
 
-var nav = new Swiper('.activity .swiper-container', {
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-    });
-
+var activity = new Swiper('.activity .swiper-container', {
+  navigation: {
+    nextEl: '.activity .swiper-button-next',
+    prevEl: '.activity .swiper-button-prev',
+  },
+});
