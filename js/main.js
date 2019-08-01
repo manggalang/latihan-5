e00dad42357dd0002f87f180279c2b97caab77ad
Mang Galang <galangpastibisa@gmail.com>
2019-08-01 22:50:43 +0800

    fix but not finished

--- js/main.js
+++ js/main.js
@@ -1,23 +1,14 @@
-var upcoming = new Swiper('.upcoming-program .swiper-container', {
-	slidesPerView: 2,
-	freeMode: true,
-});
+var swiper = new Swiper('.upcoming-program .swiper-container', {
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
 
-var upcomingNavigation = new Swiper('.upcoming-program', {
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  });
+var nav = new Swiper('.activity .swiper-container', {
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
 
-var activity = new Swiper('.activity .swiper-container', {
-	slidesPerView: 3,
-	freeMode: true,
-});
-
-var activityNavigation = new Swiper('.activity', {
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  });
