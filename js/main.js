9feb22e0819dcf6d94871fac4ef12b36753a51f5
Mang Galang <galangpastibisa@gmail.com>
2019-08-01 23:37:30 +0800

    fix finish

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 var upcomingProgram = new Swiper('.upcoming-program .swiper-container', {
+  slidesPerGroup: 2,
+  slidesPerView: 2,
   navigation: {
     nextEl: '.upcoming-program .swiper-button-next',
     prevEl: '.upcoming-program .swiper-button-prev',
@@ -6,6 +8,8 @@ var upcomingProgram = new Swiper('.upcoming-program .swiper-container', {
 });
 
 var activity = new Swiper('.activity .swiper-container', {
+  slidesPerGroup: 3,
+  slidesPerView: 3,
   navigation: {
     nextEl: '.activity .swiper-button-next',
     prevEl: '.activity .swiper-button-prev',
